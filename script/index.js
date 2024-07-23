77594018d79a465920ea0fc938eeb817a7e80e28
ofjxl <165612614+ofjxl@users.noreply.github.com>
2024-07-23 18:12:18 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -6,3 +6,7 @@ const web_s = new Swiper('.swiper',{
         prevEl: ".web .swiper-button-prev",
     },
 })
+const sns_s = new Swiper('.sns-swiper',{
+    autoplay: {delay:2000},
+    loop:true,
+})
